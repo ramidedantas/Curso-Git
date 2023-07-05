c17d9978eff91a017c7c7049b70abccc04d1dc9f
Camille <camillegomes038@gmail.com>
2023-07-05 19:26:46 -0300

    Olá!

--- hello.py
+++ hello.py
@@ -7,9 +7,6 @@ print("Doce de leite")
 print("Bardao-do-Esmaga!!!")
 print("Commit do 20!!!")
 print("Uma viagem para o futuro")
-<<<<<<< HEAD
 print("Gustavo")
-=======
 printf("To ficando maluco")
-
->>>>>>> b36bdd72383eba5160e694d6b5b543821319e181
+print("Olá, meu nome é Camille")
