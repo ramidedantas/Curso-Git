b1398ca4e7d7f0b907ef43f5a62680c7c3b5c3a1
José Neto <devneto203@gmail.com>
2023-07-05 16:41:11 -0300

    commit neto

--- hello.py
+++ hello.py
@@ -2,4 +2,7 @@ print ("Hello World!!!!!!?????")
 print ("Novo usuario")
 print("Bardao-do-Esmaga!!!")
 print("Bardao-do-Esmaga!!!")
-print("Commit do 20!!!")
+print("Commit do 20!!!!!!")
+
+nome = input("Digite seu nome: ")
+print(f"Olá {nome} Bora aprender git")
